bb56c315ecb8673e26c16ad9f260896234f32c6a
thtiiz <napon-na123@hotmail.com>
2019-01-26 18:20:37 +0700

    add success page

--- frontend/src/features/qrcode/qrcode.js
+++ frontend/src/features/qrcode/qrcode.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import QRcode from 'qrcode.react';
-
-
+import axios from 'axios';
+import firebase from 'firebase';
+let database
 class Qrcode extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: "asdfgh",
+      success: false
+    }
+    var config = {
+      apiKey: "AIzaSyC1oEPAHEbiDclvc9YboefuuDSKhkFAg_A",
+      authDomain: "aroiihack.firebaseapp.com",
+      databaseURL: "https://aroiihack.firebaseio.com",
+      projectId: "aroiihack",
+      storageBucket: "aroiihack.appspot.com",
+      messagingSenderId: "24219879467"
+    };
+    firebase.initializeApp(config);
+    console.log(firebase.database);
+    database = firebase.database;
+  }
+
+  qrCodeScan(){
+    //scan
+    
+  }
+
   render() {
     return (
       <div className="qrcode">
-        <QRcode value = "" />
-        qwe
+        {this.state.success?
+        <div className="success">
+          success
+        </div>
+        :<QRcode value = "EARN" />
+        }
+        {this.state.data}
+        {this.qrCodeScan()}
       </div>
     );
   }
